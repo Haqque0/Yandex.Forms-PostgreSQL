929e0b83da755ec478b11c0d5799feedf023c989
Haqque <161449980+Haqque0@users.noreply.github.com>
2026-03-20 16:27:40 +0300

    Add files via upload

---
+++ config_db.py
@@ -0,0 +1,15 @@
+# config_db.py
+
+# Данные для API Яндекс.Форм
+OAUTH_TOKEN = "токен"
+ORG_ID = "ID организации"
+SURVEY_ID = "ID формы"
+
+# Настройки подключения к PostgreSQL
+DB_CONFIG = {
+    "dbname": "ctf_db",
+    "user": "ctf_user",
+    "password": "qwerty",
+    "host": "localhost",
+    "port": "5432"
+}
